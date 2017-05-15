cc19b2f11dc2a6c1e40dce3c0e406809e7a05113
kanekargauri <gauri@pratilipi.com>
2017-05-15 16:19:49 +0530

    search microservice

---
+++ src/fileuploader.py
@@ -0,0 +1,182 @@
+"""
+Pratilipi Search Data Uploader
+Currently support search indexes are
+- Author
+- Pratilipi
+"""
+
+import os
+import io
+import sys
+import gzip
+import simplejson as json
+
+PATH = '/home/gauri/Downloads'
+
+
+"""
+old_os_path = os.environ.get('PATH', '')
+os.environ['PATH'] = os.path.dirname(os.path.abspath(__file__)) + os.pathsep + old_os_path
+base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if sys.platform == 'win32':
+    site_packages = os.path.join(base, 'Lib', 'site-packages')
+else:
+    site_packages = os.path.join(base, 'lib', 'python%s' % sys.version[:3], 'site-packages')
+prev_sys_path = list(sys.path)
+"""
+
+
+def readfile(filepath):
+    """
+    lazy read file line by line
+    """
+    with open(filepath, 'r') as file_obj:
+        for line in file_obj:
+            yield line.strip()
+
+
+# Authors Data
+input_file_name = PATH + '/AUTHOR'
+#input_file_name = PATH + '/test.json'
+out_file_name = PATH + '/author_solr.json.gz'
+author = {}
+line_cntr = 0
+failed_line_cntr = 0
+
+try:
+    with gzip.open(out_file_name, 'wb') as out_file_obj:
+        for line in readfile(input_file_name):
+            row = json.loads(line.strip())
+
+            try:
+                temp = {}
+                temp['author_id'] = row['AUTHOR_ID']
+                author[row['AUTHOR_ID']] = {}
+                temp['user_id'] = 0
+                if 'USER_ID' in row:
+                    temp['user_id'] = row['USER_ID']
+                    author[row['AUTHOR_ID']]['user_id'] = temp['user_id']
+                if 'FIRST_NAME' in row and row['FIRST_NAME'] is not None:
+                    temp['name'] = row['FIRST_NAME']
+                if 'LAST_NAME' in row and row['LAST_NAME'] is not None:
+                    if 'name' in temp:
+                        temp['name'] = "%s %s" % (temp['name'], row['LAST_NAME'])
+                    else:
+                        temp['name'] = row['LAST_NAME']
+                if 'name' in temp:
+                    author[row['AUTHOR_ID']]['name'] = temp['name']
+                if 'FIRST_NAME_EN' in row and row['FIRST_NAME_EN'] is not None:
+                    temp['name_en'] = row['FIRST_NAME_EN']
+                if 'LAST_NAME_EN' in row and row['LAST_NAME_EN'] is not None:
+                    if 'name_en' in temp:
+                        temp['name_en'] = "%s %s" % (temp['name_en'], row['LAST_NAME_EN'])
+                    else:
+                        temp['name_en'] = row['LAST_NAME_EN']
+                if 'name_en' in temp:
+                    author[row['AUTHOR_ID']]['name_en'] = temp['name_en']
+                temp['content_published'] = row.get('CONTENT_PUBLISHED', 0)
+                temp['follow_count'] = row.get('FOLLOW_COUNT', 0)
+                if 'LANGUAGE' in row and row['LANGUAGE'] is not None:
+                    temp['lang'] = row['LANGUAGE']
+                temp['state'] = row['STATE']
+                if 'GENDER' in row and row['GENDER'] is not None:
+                    temp['gender'] = row['GENDER']
+                if 'PEN_NAME' in row and row['PEN_NAME'] is not None:
+                    temp['pen_name'] = row['PEN_NAME']
+                if 'PEN_NAME_EN' in row and row['PEN_NAME_EN'] is not None:
+                    temp['pen_name_en'] = row['PEN_NAME_EN']
+                if 'SUMMARY' in row and row['SUMMARY'] is not None:
+                    temp['summary'] = row['SUMMARY']
+                temp['read_count'] = row.get('TOTAL_READ_COUNT', 0)
+                temp['reg_date'] = row['REGISTRATION_DATE']
+                if 'PROFILE_IMAGE' in row and row['PROFILE_IMAGE'] is not None:
+                    temp['profile_image'] = row['PROFILE_IMAGE']
+                if 'COVER_IMAGE' in row and row['COVER_IMAGE'] is not None:
+                    temp['cover_image'] = row['COVER_IMAGE']
+            except Exception as err:
+                print "Failed while procesing Author line - ", str(err), line
+                failed_line_cntr = line_cntr + 1
+                continue
+
+            try:
+                outLine = json.dumps(temp)
+                out_file_obj.write(outLine)
+                out_file_obj.write('\n')
+            except Exception as err:
+                print "Failed while writing to Author output file - ", str(err), line
+                failed_line_cntr = line_cntr + 1
+                continue
+
+            line_cntr = line_cntr + 1
+except Exception as err:
+    print "Failed while reading Author data - ", str(err)
+
+
+print "Author Total: ", line_cntr, " Failed: ", failed_line_cntr
+
+#sys.exit(1)
+
+# Pratilipi Data
+input_file_name = PATH + '/PRATILIPI'
+#input_file_name = PATH + '/pratilipi.json'
+out_file_name = PATH + '/pratilipi_solr.json.gz'
+line_cntr = 0
+failed_line_cntr = 0
+
+try:
+    with gzip.open(out_file_name, 'wb') as out_file_obj:
+        for line in readfile(input_file_name):
+            row = json.loads(line.strip())
+
+            try:
+                temp = {}
+                temp['pratilipi_id'] = row['PRATILIPI_ID']
+                temp['author_id'] = row['AUTHOR_ID']
+                temp['chapter_count'] = row.get('CHAPTER_COUNT', 0)
+                if 'COVER_IMAGE' in row and row['COVER_IMAGE'] is not None:
+                    temp['cover_image'] = row['COVER_IMAGE']
+                if 'LANGUAGE' in row and row['LANGUAGE'] is not None:
+                    temp['lang'] = row['LANGUAGE']
+                temp['state'] = row['STATE']
+                temp['listing_date'] = row['LISTING_DATE']
+                temp['content_type'] = row['CONTENT_TYPE']
+                temp['pratilipi_type'] = row['PRATILIPI_TYPE']
+                temp['rating_count'] = row.get('RATING_COUNT', 0)
+                temp['review_count'] = row.get('REVIEW_COUNT', 0)
+                temp['read_count'] = row.get('READ_COUNT', 0)
+                if 'SUMMARY' in row and row['SUMMARY'] is not None:
+                    temp['summary'] = row['SUMMARY']
+                if 'TITLE' in row and row['TITLE'] is not None:
+                    temp['title'] = row['TITLE']
+                if 'TITLE_EN' in row and row['TITLE_EN'] is not None:
+                    temp['title_en'] = row['TITLE_EN']
+                temp['total_rating'] = row.get('TOTAL_RATING', 0)
+                temp['genre'] = None
+                if row['AUTHOR_ID'] in author:
+                    if 'name' in author[row['AUTHOR_ID']]:
+                        temp['author_name'] = author[row['AUTHOR_ID']]['name']
+                    if 'name_en' in author[row['AUTHOR_ID']]:
+                        temp['author_name_en'] = author[row['AUTHOR_ID']]['name_en']
+                    temp['user_id'] = author[row['AUTHOR_ID']]['user_id']
+            except Exception as err:
+                print "Failed while procesing Pratilipi line - ", str(err), line
+                failed_line_cntr = failed_line_cntr + 1
+                continue
+
+            try:
+                outLine = json.dumps(temp)
+                out_file_obj.write(outLine)
+                out_file_obj.write('\n')
+            except Exception as err:
+                print "Failed while writing to Pratilipi output file - ", str(err), line
+                failed_line_cntr = failed_line_cntr + 1
+                continue
+
+            line_cntr = line_cntr + 1
+except Exception as err:
+    print "Failed while reading Pratilipi data - ", str(err)
+
+
+print "Pratilipi Total: ", line_cntr, " Failed: ", failed_line_cntr
+
+
---
+++ src/main.py
@@ -0,0 +1,250 @@
+"""Search Microservice v1.0
+Primary gateway to get data from pratilipi data store
+currently supports search of -
+- authors
+- pratilipis (content)
+"""
+
+import ConfigParser
+import requests
+import os
+import sys
+import inspect
+import simplejson as json
+import re
+
+from datetime import datetime
+from bottle import route, run, request, response, template
+
+
+# set the base path
+base_path = os.path.abspath(os.path.dirname(__file__))
+config_path = "%s/%s" % (base_path.replace('src', 'config'), "search.cnf")
+
+
+# fetch config
+search_config = ConfigParser.RawConfigParser()
+search_config.read(config_path)
+url_path = search_config.get("SOLR", "BASE_URL")
+
+
+def api_response(status_code, msg, data=None):
+    """
+    api response formatter
+    """
+    response = {}
+    response["response_header"] = {"status_code": status_code, "msg": msg}
+    if data is not None:
+        response["response"] = data
+    return response
+
+
+def log_formatter(fname, msg):
+    return "[%s] %s, %s" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), fname, msg) 
+
+
+@route('/ping')
+def ping():
+    """
+    ping - to check if the api is up
+    """
+    return log_formatter(inspect.stack()[0][3], "pong")
+
+
+@route('/create_core')
+def create_core():
+    """
+    create a new core
+    """
+    name = request.query['name']
+    url = "%s/solr/admin/cores?action=CREATE&name=%s" % (url_path, name)
+    req = requests.get(url)
+    if req.status_code == 200:
+        return api_response(req.status_code, req.text)
+    return api_response(req.status_code, req.text)
+
+
+@route('/add_row', method='POST')
+def add_row():
+    """
+    add an entry to solr core
+    """
+    name = request.forms['name']
+    data = request.forms['data']
+
+    row = { "add": {
+        "doc": "data",
+        "commitWithin": 5,
+        "overwrite": "false" } }
+    
+    url = "%s/solr/%s/update?commit=true" % (url_path, name)
+    print "=====> ", url
+    req = requests.post(
+        url, data, {":content_type": "application/json", ":accept": "application/json"})
+    return api_response(req.status_code, req.text)
+
+
+
+@route('/delete')
+def update(core_name, data):
+    """
+    update an entry to solr core
+    """
+    name = request.forms['name']
+    data = request.forms['data']
+
+    url = url_path + "/solr/%s/update?commit=true" % core_name
+    req = requests.post(
+        url, data, {":content_type": "application/json", ":accept": "application/json"})
+    return api_response(req.status_code, req.text)
+
+
+@route('/update')
+def delete(core_name, data):
+    """
+    delete an entry from solr core
+    """
+    url = url_path + "/solr/%s/update?commit=true" % core_name
+    req = requests.post(
+        url, data, {":content_type": "application/json", ":accept": "application/json"})
+    return api_response(req.status_code, req.text)
+
+
+@route('/file_uploader')
+def file_uploader():
+    """
+    upload file in batches to solr core
+    """
+    name = request.query['name']
+    filename = request.query['filename']
+
+    response = fileuploader()
+    return api_response(response['status_code'], response['text'], response['data'])
+
+
+
+@route('/search')
+def search():
+    """
+    search
+    """
+    accepttxt = request.headers.get('Accept', "version=1.0")
+    searchtxt = re.search( r'version=(\d.\d)', accepttxt.lower())
+    version = 1.0
+    if searchtxt:
+        version = searchtxt.group()[-3:]
+
+    userid = request.query.get('userid', None)
+    text = request.query.get('text', None)
+    lang = request.query.get('lang', None)
+    is_active = request.query.get('is_active', True)
+    limit = request.query.get('limit', 20)
+    offset = request.query.get('offset', 0)
+
+    if text is None:
+        return api_response(400, "Bad Request")
+        
+    print log_formatter(inspect.stack()[0][3], "%s %s %s %s %s" % (userid, version, limit, offset, text))
+
+    try:
+        pratilipi_count = 0
+        author_count = 0
+
+        #lang, read_count, name, gender, pen_name_en, follow_count, 
+        #profile_image, content_published, pen_name
+        #reg_date, state, cover_image, name_en, summary, author_id
+
+        state_filter = " AND state:ACTIVE" if is_active else ""
+        lang_filter = " AND lang:%s" % lang if lang is not None else ""
+        url = url_path 
+        url = "%s/solr/author/select" % url
+        url = "%s?wt=json&q=*%s*%s%s" % (url, text, lang_filter, state_filter)
+        url = "%s&fl=name,name_en,summary,score,read_count,author_id" % url
+        url = "%s&sort=score desc,read_count desc" % url
+        url = "%s&rows=%s&start=%s" % (url, limit, offset)
+        #url = "%s&qf=name^200+name_en^200+summary^0.1" % url
+
+        print log_formatter(inspect.stack()[0][3], "solr url %s" % url)
+
+        author = []
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+        
+            author_count = data['response']['numFound']
+            for row in data['response']['docs']:
+                temp = {}
+                temp['author_id'] = row['author_id']
+                temp['score'] = row['score']
+                temp['read_count'] = row['read_count']
+                if 'name' in row: 
+                    temp['name'] = row['name']
+                if 'name_en' in row:
+                    temp['name_en'] = row['name_en']
+                if 'summary' in row:
+                    temp['summary'] = row['summary']
+                author.append(temp)
+
+
+        #lang, read_count, pratilipi_type, review_count, total_rating, listing_date, title, title_en, state, 
+        # author_name, summary, genre, chapter_count, rating_count, content_type, cover_image, author_id, author_name_en
+        state_filter = " AND state:PUBLISHED" if is_active else ""
+        lang_filter = " AND lang:%s" % lang if lang is not None else ""
+        url = url_path 
+        url = url_path
+        url = "%s/solr/pratilipi/select" % url
+        url = "%s?wt=json&q=*%s*%s%s" % (url, text, lang_filter, state_filter)
+        url = "%s&fl=title,title_en,summary,score,read_count,author_id" % url
+        url = "%s,author_name,author_name_en,genre,pratilipi_id" % url
+        url = "%s&sort=score desc,read_count desc" % url
+        url = "%s&rows=%s&start=%s" % (url, limit, offset)
+
+
+        print log_formatter(inspect.stack()[0][3], "solr url %s" % url)
+        pratilipi = []
+        pratilipi_count = 0
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+
+            pratilipi_count = data['response']['numFound']
+            for row in data['response']['docs']:
+                temp = {}
+                temp['author_id'] = row['author_id']
+                temp['pratilipi_id'] = row['pratilipi_id']
+                temp['score'] = row['score']
+                temp['read_count'] = row['read_count']
+                if 'title' in row:
+                    temp['title'] = row['title']
+                if 'title_en' in row:
+                    temp['title_en'] = row['title_en']
+                if 'author_name' in row:
+                    temp['author_name'] = row['author_name']
+                if 'author_name_en' in row:
+                    temp['author_name_en'] = row['author_name_en']
+                if 'summary' in row:
+                    temp['summary'] = row['summary']
+                pratilipi.append(temp)
+
+        
+        response = {}
+        if author_count > 0:
+            response['authors_found'] = author_count
+            response['authors'] = author
+        if pratilipi_count > 0:
+            response['pratilipis_found'] = pratilipi_count
+            response['pratilips'] = pratilipi
+
+        if not bool(response):
+            return api_response(204, "No Content")
+
+        print log_formatter(inspect.stack()[0][3], "search done")
+        return api_response(200, "Success", response)
+    except Exception as err:
+        print "failed while searching - ", str(err)
+        return api_response(500, 'Internal Server Error')
+
+
+run(host='127.0.0.1', port='2579')
+
+

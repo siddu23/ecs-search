75457e3d1b5e94599d0717759df2bc2fc66f9345
gauri <gauri@pratilipi.com>
2017-06-29 18:46:08 +0530

    passing userid to services via param and not header

--- src/v1.py
+++ src/v1.py
@@ -147,13 +147,11 @@ def author_data(config_dict, pdict):
         if author_count > 0 and pdict['author_offset'] < author_count:
             response['authors_found'] = author_count
             url = "{}".format(config_dict['author_url'])
-            param_dict = {'ids':str(author)}
-            headers = {"User-Id": str(pdict['userid'])}
+            param_dict = {'ids':str(author), 'userId':pdict['userid']}
             print log_formatter(inspect.stack()[0][3], "called author service")
             print log_formatter(inspect.stack()[0][3], param_dict)
-            print log_formatter(inspect.stack()[0][3], headers)
             
-            service_response = requests.get(url, params=param_dict, headers=headers)
+            service_response = requests.get(url, params=param_dict)
             print log_formatter(inspect.stack()[0][3], "done author service")
             if service_response.status_code == 200:
                 response = {'authorList': [], 'authorCursor': pdict['author_offset'] + pdict['author_limit'] + 1, 'numberFound': author_count}
@@ -198,10 +196,8 @@ def pratilipi_data(config_dict, pdict):
         #prepare pratilipi dict
         pratilipi = []
         pratilipi_count = 0
-        headers = {"User-Id": str(pdict['userid'])}
         print log_formatter(inspect.stack()[0][3], "pratilipi headers")
-        print log_formatter(inspect.stack()[0][3], headers)
-        response = requests.get(url, params=param_dict, headers=headers)
+        response = requests.get(url, params=param_dict)
         if response.status_code == 200:
             data = json.loads(response.text)
             pratilipi_count = data['response']['numFound']
@@ -213,7 +209,7 @@ def pratilipi_data(config_dict, pdict):
         if pratilipi_count > 0 and pdict['pratilipi_offset'] < pratilipi_count:
             response['pratilipis_found'] = pratilipi_count
             url = "{}".format(config_dict['pratilipi_url'])
-            param_dict = {'ids':str(pratilipi)}
+            param_dict = {'ids':str(pratilipi), 'userId':pdict['userid']}
             print log_formatter(inspect.stack()[0][3], "called pratilipi service")
             print log_formatter(inspect.stack()[0][3], param_dict)
             service_response = requests.get(url, params=param_dict)

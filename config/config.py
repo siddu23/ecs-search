ae29bda36504f274aab90c62ee451c1f2370e68e
gauri <gauri@pratilipi.com>
2017-07-21 20:06:05 +0530

    adapt to API_END_POINT env var change

--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
 import os
 
 SOLR_URL = "http://ip-172-31-16-221.ap-southeast-1.compute.internal:8983/solr"
-PRATILIPI_SERVICE_URL = "http://{}/{}".format(os.environ['API_END_POINT'], "pratilipis")
-AUTHOR_SERVICE_URL = "http://{}/{}".format(os.environ['API_END_POINT'], "authors")
+PRATILIPI_SERVICE_URL = "{}/{}".format(os.environ['API_END_POINT'], "pratilipis")
+AUTHOR_SERVICE_URL = "{}/{}".format(os.environ['API_END_POINT'], "authors")
 REDIS_URL = "ecs-search.e6ocw5.0001.apse1.cache.amazonaws.com"
 REDIS_PORT = 8080
 REDIS_DB = 9

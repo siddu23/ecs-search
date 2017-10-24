33b8ec497e922f3137ff72b6e7280187359b4a73
gauri <gauri@pratilipi.com>
2017-10-24 14:00:24 +0530

    changed redis host

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import os
 SOLR_URL = "http://ip-172-31-16-221.ap-southeast-1.compute.internal:8983/solr"
 PRATILIPI_SERVICE_URL = "{}/{}".format(os.environ['API_END_POINT'], "pratilipis")
 AUTHOR_SERVICE_URL = "{}/{}".format(os.environ['API_END_POINT'], "authors")
-REDIS_URL = "ecs-search.e6ocw5.0001.apse1.cache.amazonaws.com"
+REDIS_URL = "devo-ecs.e6ocw5.0001.apse1.cache.amazonaws.com"
 REDIS_PORT = 8080
 REDIS_DB = 9
 TOP_SEARCH_LIMIT = 10
@@ -11,13 +11,13 @@ TOP_SEARCH_AGE_IN_MIN = 3600
 
 if os.environ["STAGE"] == "gamma":
     SOLR_URL = "http://ip-172-31-0-99.ap-southeast-1.compute.internal:8983/solr"
-    REDIS_URL = "ecs-search-001.cpzshl.0001.apse1.cache.amazonaws.com"
+    REDIS_URL = "prod-ecs-001.cpzshl.0001.apse1.cache.amazonaws.com"
 elif os.environ["STAGE"] == "prod":
     SOLR_URL = "http://ip-172-31-0-99.ap-southeast-1.compute.internal:8983/solr"
-    REDIS_URL = "ecs-search-001.cpzshl.0001.apse1.cache.amazonaws.com"
+    REDIS_URL = "prod-ecs-001.cpzshl.0001.apse1.cache.amazonaws.com"
 elif os.environ["STAGE"] == "devo":
     SOLR_URL = "http://ip-172-31-16-221.ap-southeast-1.compute.internal:8983/solr"
-    REDIS_URL = "ecs-search.e6ocw5.0001.apse1.cache.amazonaws.com"
+    REDIS_URL = "devo-ecs.e6ocw5.0001.apse1.cache.amazonaws.com"
 elif os.environ["STAGE"] == "local":
     SOLR_URL = "http://localhost:8983/solr"
     REDIS_URL = "localhost"
